764677c269b30f36a64cbe251ec74a5a8edd88ff
JakubSto <stolarczyk.jakub@gmail.com>
2019-06-04 21:16:18 +0200

    optimization

--- src/DayOfWeek.java
+++ src/DayOfWeek.java
@@ -11,7 +11,7 @@ public enum DayOfWeek {
 
     private String polishName;
     private boolean workDay;
-  
+
     DayOfWeek(String polishName, boolean workDay){
         this.polishName = polishName;
         this.workDay = workDay;
@@ -27,18 +27,17 @@ public enum DayOfWeek {
 
     public static ArrayList workDays(){
         ArrayList<DayOfWeek> tempDaysList = new ArrayList<>();
-        for (DayOfWeek day: DayOfWeek.values()) {
-            if (day.workDay == true){
+        for (DayOfWeek day: DayOfWeek.values())
+            if (day.workDay) {
                 tempDaysList.add(day);
             }
-        }
         return tempDaysList;
     }
 
     public static ArrayList weekendDays(){
         ArrayList<DayOfWeek> tempDaysList = new ArrayList<>();
         for (DayOfWeek day: DayOfWeek.values()) {
-            if (day.workDay == false){
+            if (day.workDay){
                 tempDaysList.add(day);
             }
         }
