729a22acbedc0d33e57bfd87f2a308b55cf4e0a6
JakubSto <stolarczyk.jakub@gmail.com>
2019-06-04 21:06:02 +0200

    second

--- src/DayOfWeek.java
+++ src/DayOfWeek.java
@@ -1,2 +1,59 @@
+import java.util.ArrayList;
+
 public enum DayOfWeek {
+    MONDAY("Poniedziałek", true),
+    TUESDAY("Wtorek", true),
+    WEDNESDAY("środa", true),
+    THURSDAY("Czwartek", true),
+    FRIDAY("Piątek", true),
+    SATURDAY("Sobota",false),
+    SUNDAY("Niedziela", false);
+
+    private String polishName;
+    private boolean workDay;
+    ArrayList<DayOfWeek> workDayList = new ArrayList<>();
+    ArrayList<DayOfWeek> weekendsDayList = new ArrayList<>();
+    ArrayList<String> polishNameDaysList = new ArrayList<>();
+
+
+    DayOfWeek(String polishName, boolean workDay){
+        this.polishName = polishName;
+        this.workDay = workDay;
+    }
+
+    public String getPolishName() {
+        return polishName;
+    }
+
+    public boolean isWorkDay() {
+        return workDay;
+    }
+
+    public static ArrayList workDays(){
+        ArrayList<DayOfWeek> tempDaysList = new ArrayList<>();
+        for (DayOfWeek day: DayOfWeek.values()) {
+            if (day.workDay == true){
+                tempDaysList.add(day);
+            }
+        }
+        return tempDaysList;
+    }
+
+    public static ArrayList weekendDays(){
+        ArrayList<DayOfWeek> tempDaysList = new ArrayList<>();
+        for (DayOfWeek day: DayOfWeek.values()) {
+            if (day.workDay == false){
+                tempDaysList.add(day);
+            }
+        }
+        return tempDaysList;
+    }
+
+    public static ArrayList polishNameDays(){
+        ArrayList<String> tempDaysList = new ArrayList<>();
+        for (DayOfWeek day: DayOfWeek.values()) {
+            tempDaysList.add(day.polishName);
+        }
+        return tempDaysList;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(DayOfWeek.polishNameDays());
+        System.out.println(DayOfWeek.weekendDays());
+        System.out.println(DayOfWeek.workDays());
+
+        for (DayOfWeek day: DayOfWeek.values())
+            {
+                System.out.println();
+                System.out.println(day);
+            }
     }
 }
